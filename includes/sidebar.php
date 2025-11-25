c86e2ee1694487d760f109f495ca47c6c677fffc
danielle-brooks <danielle04brooks@gmail.com>
2025-11-24 21:09:36 -0500

    Update sidebar.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,5 +1,5 @@
 <aside class="sidebar">
-  <a href="#" class="load-page" data-page="home.php">Home</a>
+  <a href="#" class="load-page" data-page="dashboard.php">Home</a>
   <a href="#" class="load-page" data-page="new_contact.php">New Contact</a>
   <a href="#" class="load-page" data-page="users.php">Users</a>
   <a href="logout.php">Logout</a>
